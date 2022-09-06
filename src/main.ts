831649ed182028672e3056a6bea335559c8afca2
hyeonjeong <hj.lee@fromthered.com>
2022-09-06 17:30:15 +0900

    ..

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,7 @@ async function bootstrap() {
   app.use("/upload", express.static(path.join(process.cwd(), "upload")));
   app.enableCors();
 
-  await app.listen(5001);
+  await app.listen(process.env.SERVER_PORT);
+
 }
 bootstrap();
